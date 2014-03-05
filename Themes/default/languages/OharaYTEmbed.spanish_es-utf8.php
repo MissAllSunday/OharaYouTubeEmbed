96f7447171562519564fdd5bf2ebbcd719bf5ef3
Suki <suki@missallsunday.com>
2014-03-05 10:20:13 -0600

    Shorter license header, typos and all that boring stuff...
    Signed-off-by: Suki <suki@missallsunday.com>

--- Themes/default/languages/OharaYTEmbed.spanish_es-utf8.php
+++ Themes/default/languages/OharaYTEmbed.spanish_es-utf8.php
@@ -1,35 +1,11 @@
 <?php
 
-/**
- * @package Ohara Youtube Embed mod
- * @version 1.1
- * @author Jessica González <missallsunday@simplemachines.org>
- * @copyright Copyright (C) 2011, 2012, 2013, Jessica González
- * @license http://www.mozilla.org/MPL/MPL-1.1.html
- */
-
 /*
- * Version: MPL 1.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is http://missallsunday.com code.
- *
- * The Initial Developer of the Original Code is
- * Jessica González.
- * Portions created by the Initial Developer are Copyright (C) 2011, 2012, 2013,
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *
+ * @package Ohara Youtube Embed mod
+ * @version 1.2
+ * @author Jessica González <missallsunday@simplemachines.org>
+ * @copyright Copyright (C) 2011, 2012, 2013, 2014 Jessica González
+ * @license http://www.mozilla.org/MPL/MPL-1.1.html
  */
 
 global $txt;
@@ -38,7 +14,7 @@ $txt['OYTE_master'] = 'Activar el mod Ohara Youtube Embed';
 $txt['OYTE_master_sub'] = 'Esta es la configuración principal, necasita estar activada para que el mod funcione correctamente.';
 $txt['OYTE_video_width'] = 'Ancho del video:';
 $txt['OYTE_video_width_sub'] = 'Si se deja vacio se usará el valor por deault: 420.';
-$txt['OYTE_video_height'] = 'Alura del video:';
+$txt['OYTE_video_height'] = 'Altura del video:';
 $txt['OYTE_video_height_sub'] = 'Si se deja vacio se usará el valor por defecto: 315';
 $txt['OYTE_desc'] = 'Inserta un video desde youtube';
 $txt['OYTE_vimeo_desc'] = 'Inserta un video desde vimeo';
--- removeHooks.php
+++ removeHooks.php
@@ -1,35 +1,11 @@
 <?php
 
-/**
- * @package Ohara Youtube Embed mod
- * @version 1.1
- * @author Jessica González <missallsunday@simplemachines.org>
- * @copyright Copyright (C) 2011, 2012, 2013, Jessica González
- * @license http://www.mozilla.org/MPL/MPL-1.1.html
- */
-
 /*
- * Version: MPL 1.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is http://missallsunday.com code.
- *
- * The Initial Developer of the Original Code is
- * Jessica González.
- * Portions created by the Initial Developer are Copyright (C) 2011, 2012, 2013,
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *
+ * @package Ohara Youtube Embed mod
+ * @version 1.2
+ * @author Jessica González <missallsunday@simplemachines.org>
+ * @copyright Copyright (C) 2011, 2012, 2013, 2014 Jessica González
+ * @license http://www.mozilla.org/MPL/MPL-1.1.html
  */
 
 if (file_exists(dirname(__FILE__) . '/SSI.php') && !defined('SMF'))
