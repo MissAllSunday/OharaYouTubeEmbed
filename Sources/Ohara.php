11e3a1cdd705c6549a190e41d3d8ddbb56df97e4
Suki <suki@missallsunday.com>
2014-07-07 15:10:58 -0500

    This is now a class and it also extends Ohara
    Signed-off-by: Suki <suki@missallsunday.com>

---
+++ Sources/Ohara.php
@@ -0,0 +1,113 @@
+<?php
+
+/**
+ * @package Ohara helper class
+ * @version 1.0
+ * @author Jessica González <suki@missallsunday.com>
+ * @copyright Copyright (c) 2014, Jessica González
+ * @license http://www.mozilla.org/MPL/2.0/
+ */
+
+namespace Suki;
+
+class Ohara
+{
+	public static $name = '';
+
+	public function text($var)
+	{
+		global $txt;
+
+		// This should be extended by somebody else...
+		if (empty(static::$name))
+			return false;
+
+		// No var to check.
+		if (empty($var))
+			return false;
+
+		// Load the mod's language file.
+		loadLanguage(static::$name);
+
+		if (!empty($txt[static::$name .'_'. $var]))
+			return $txt[static::$name .'_'. $var];
+
+		else
+			return false;
+	}
+
+	public function enable($var)
+	{
+		global $modSettings;
+
+		if (empty($var))
+			return false;
+
+		if (isset($modSettings[static::$name .'_'. $var]) && !empty($modSettings[static::$name .'_'. $var]))
+			return true;
+
+		else
+			return false;
+	}
+
+	public function setting($var)
+	{
+		global $modSettings;
+
+		// This should be extended by somebody else...
+		if (empty(static::$name))
+			return false;
+
+		if (empty($var))
+			return false;
+
+		global $modSettings;
+
+		if (true == $this->enable($var))
+			return $modSettings[static::$name .'_'. $var];
+
+		else
+			return false;
+	}
+
+	public function data($var)
+	{
+		return $this->validate($var) ? $this->sanitize($this->_request[$var]) : false;
+	}
+
+	public function validate($var, $type = 'request')
+	{
+		$this->_types = array('request' => $_REQUEST, 'get' => $_GET, 'post' => $_POST);
+
+		$this->_request = (empty($type) || !isset($types[$type])) ? $_REQUEST : $types[$type];
+
+		return (isset($this->_request[$var]));
+	}
+
+	public function sanitize($var)
+	{
+		global $smcFunc;
+
+		if (is_array($var))
+		{
+			foreach ($var as $k => $v)
+				$var[$k] = $this->sanitize($v);
+
+			return $var;
+		}
+
+		else
+		{
+			if (is_numeric($var))
+				$var = (int)trim($var);
+
+			else if (is_string($var))
+				$var = $smcFunc['htmltrim']($smcFunc['htmlspecialchars']($var), ENT_QUOTES);
+
+			else
+				$var = 'error_' . $var;
+		}
+
+		return $var;
+	}
+}
