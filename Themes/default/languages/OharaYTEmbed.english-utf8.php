c4a844f1cc07965d673489849d7d0e29d269e153
Suki <suki@missallsunday.com>
2014-07-08 10:16:52 -0500

    The always annoying task of adding new language strings..
    Signed-off-by: Suki <suki@missallsunday.com>

--- Themes/default/languages/OharaYTEmbed.english-utf8.php
+++ Themes/default/languages/OharaYTEmbed.english-utf8.php
@@ -12,6 +12,8 @@ global $txt;
 
 $txt['OharaYTEmbed_enable'] = 'Enable the Ohara Youtube Embed mod';
 $txt['OharaYTEmbed_enable_sub'] = 'This is the master setting, it needs to be enable for the mod to work.';
+$txt['OharaYTEmbed_autoEmbed'] = 'Enable the autoembed feature';
+$txt['OharaYTEmbed_autoEmbed_sub'] = 'If enable, all valid youtub anbd vimeo links posted will be converted to embedded videos automatically. If disabled you will have to use the BBC tags to convert links to videos.';
 $txt['OharaYTEmbed_width'] = 'Video\'s width:';
 $txt['OharaYTEmbed_width_sub'] = 'If empty it will take the default value: 420.';
 $txt['OharaYTEmbed_height'] = 'Video\'s height:';
--- Themes/default/languages/OharaYTEmbed.spanish_es.php
+++ Themes/default/languages/OharaYTEmbed.spanish_es.php
@@ -12,6 +12,8 @@ global $txt;
 
 $txt['OharaYTEmbed_enable'] = 'Activar el mod Ohara Youtube Embed';
 $txt['OharaYTEmbed_enable_sub'] = 'Esta es la configuraci&oacute;n principal, necasita estar activada para que el mod funcione correctamente.';
+$txt['OharaYTEmbed_autoEmbed'] = 'Activar la auto conversi&oacute;n';
+$txt['OharaYTEmbed_autoEmbed_sub'] = 'Si se activa esta opci&oacute;n todos los enlaces  de youtube y vimeo que sean v&aacute;lidos ser&aacute;n convertidos a videos. Si se desactiva deber&aacute;s de usar los tags correspondientes para convertir los enlaces en videos';
 $txt['OharaYTEmbed_width'] = 'Ancho del video:';
 $txt['OharaYTEmbed_width_sub'] = 'Si se deja vacio se usar&aacute; el valor por deault: 420.';
 $txt['OharaYTEmbed_height'] = 'Altura del video:';
