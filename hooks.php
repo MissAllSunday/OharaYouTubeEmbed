dfbabd0f44aa5f21affb5fed2c2d79fa5b786e5c
Infernus <egorzik2@gmail.com>
2014-12-12 13:54:15 +0100

    - Hooks clean up

---
+++ hooks.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * @package Ohara Youtube Embed mod
+ * @version 2.0
+ * @author Jessica González <missallsunday@simplemachines.org>
+ * @copyright Copyright (C) 2014 Jessica González
+ * @license http://www.mozilla.org/MPL/MPL-1.1.html
+ */
+
+    if(!defined('SMF'))
+        die('<b>Error:</b> Cannot install - please verify you put this in the same place as SMF\'s index.php.');
+
+    if (file_exists(dirname(__FILE__) . '/SSI.php'))
+        require_once(dirname(__FILE__) . '/SSI.php');
+
+    if (version_compare(PHP_VERSION, '5.3.0', '<'))
+        exit('This mod needs PHP 5.3 or greater. You will not be able to install/use this mod, contact your host and ask for a php upgrade.');
+
+    $hooks = array(
+        'integrate_bbc_codes'               => '$sourcedir/OharaYTEmbed.php|OharaYTEmbed::code#',
+        'integrate_bbc_buttons'             => '$sourcedir/OharaYTEmbed.php|OharaYTEmbed::button#',
+        'integrate_general_mod_settings'    => '$sourcedir/OharaYTEmbed.php|OharaYTEmbed::settings#',
+        'integrate_credits'                 => '$sourcedir/OharaYTEmbed.php|OharaYTEmbed::who#',
+        'integrate_pre_parsebbc'            => '$sourcedir/OharaYTEmbed.php|OharaYTEmbed::autoEmbed#',
+        'integrate_load_theme'              => '$sourcedir/OharaYTEmbed.php|OharaYTEmbed::css#',
+    );
+
+    if (!empty($context['uninstalling'])) {
+        $call = 'remove_integration_function';
+    } else {
+        $call = 'add_integration_function';
+    }
+
+    foreach ($hooks as $hook => $function)
+        $call($hook, $function);
