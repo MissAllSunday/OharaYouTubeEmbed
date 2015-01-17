34df0550c0583de729e8952797e5f9c0602a4fb3
Suki <suki@missallsunday.com>
2015-01-17 15:42:02 -0600

    Create the Vimeo file
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/OharaYTEmbed/OHVimeo.php
+++ Sources/OharaYTEmbed/OHVimeo.php
@@ -6,13 +6,12 @@ if (!defined('SMF'))
 class OHYouTube extends OharaYTEmbed
 {
 	public $siteSettings = array(
-		'identifier' => 'youtube',
-		'name' => 'You Tube',
-		'code' => 'youtube',
-		'extra_tag' => 'yt',
+		'identifier' => 'vimeo',
+		'name' => 'Vimeo',
+		'code' => 'vimeo',
 		'js_inline' => '',
-		'js_file' => 'ohyoutube.js',
-		'regex' => '~(?<=[\s>\.(;\'"]|^)(?:http|https):\/\/[\w\-_%@:|]?(?:www\.)?(?:youtu\.be/|youtube\.com(?:/embed/|/v/|/watch\?v=|/watch\?.+&v=))([\w-]{11})(?=[^\w-]|$)(?![?=&+%\w.-]*(?:[\'"][^<>]*>  | </a>  ))[?=&+%\w.-]*[/\w\-_\~%@\?;=#}\\\\]?~ix',
+		'js_file' => '',
+		'regex' => '',
 	);
 
 	public function __construct()
@@ -36,31 +35,38 @@ class OHYouTube extends OharaYTEmbed
 		//Set a local var for laziness.
 		$result = '';
 
-		// We all love Regex.
-		$pattern = '#(?:https?:\/\/)?(?:www\.)?(?:youtu\.be/|youtube\.com(?:/embed/|/v/|/watch\?v=|/watch\?.+&v=))([\w-]{11})(?:.+)?$#x';
+		// First try, pure regex.
+		$pattern = '/(?:https?:\/\/)?(?:www\.)?(?:player\.)?vimeo\.com\/(?:[a-z]*\/)*([0-9]{6,11})[?]?.*/';
 
-		// First attempt, pure regex.
+		// Get the video ID.
 		if (preg_match($pattern, $data, $matches))
-			$result = isset($matches[1]) ? $matches[1] : false;
+			$videoID = isset($matches[1]) ? $matches[1] : false;
 
-		// Give another regex a chance.
-		elseif (empty($result) && preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $data, $match))
-			$result = isset($match[1]) ? $match[1] : false;
+		// Got anything?
+		if (!empty($videoID) && ctype_digit($videoID))
+			return $this->create($videoID);
 
-		// No?, then one last chance, let PHPs native parse_url() function do the dirty work.
-		elseif (empty($result))
+		// Nope? then fall back to vimeo's API.
+		else
 		{
-			// This relies on the url having ? and =, this is only an emergency check.
-			parse_str(parse_url($data, PHP_URL_QUERY), $result);
-			$result = isset($result['v']) ? $result['v'] : false;
+			// Need a function in a far far away file...
+			require_once($this->sourceDir .'/Subs-Package.php');
+
+			// Construct the URL
+			$oembed = '//vimeo.com/api/oembed.json?url=' . rawurlencode($data) . '&width='. ($this->width) .'&height='. ($this->height);
+
+			//Attempts to fetch data from a URL, regardless of PHP's allow_url_fopen setting
+			$jsonArray = json_decode(fetch_web_data($oembed), true);
+
+			if (!empty($jsonArray) && is_array($jsonArray) && !empty($jsonArray['html']))
+				return $jsonArray['html'];
+
+			else
+				return str_replace('{site}', $this->siteSettings['name'], $that->text('invalid_link'));
 		}
 
-		// At this point, all tests had miserably failed.
-		if (empty($result))
-			return $data;
-
-		// Got something, return it!.
-		return $this->create($result);
+		// If we reach this place, it means everything else failed miserably...
+		return sprintf($txt['OYTE_unvalid_link'], 'vimeo');
 	}
 
 	public function auto(&$message)
@@ -90,6 +96,7 @@ class OHYouTube extends OharaYTEmbed
 
 	public function create($videoID)
 	{
-		return !empty($videoID) ? '<div class="oharaEmbed youtube" id="'. $videoID .'" style="width: '. $this->width .'px; height: '. $this->height .'px;"></div>' : '';
+		return !empty($videoID) ? '<div class="oharaEmbed"><iframe width="'. $that->width .'" height="'. $that->height .'" src="//player.vimeo.com/video/'. $videoID .'" frameborder="0"></iframe></div>' : '';
+		};
 	}
 }
