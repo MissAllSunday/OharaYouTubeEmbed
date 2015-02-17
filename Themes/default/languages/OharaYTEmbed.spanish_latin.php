3ca51dd1fe6382acb134d6b2d03f1cf21a0b4b0b
Suki <suki@missallsunday.com>
2015-02-17 10:36:22 -0600

    Re-do the language strings
    Signed-off-by: Suki <suki@missallsunday.com>

--- Themes/default/languages/OharaYTEmbed.spanish_latin.php
+++ Themes/default/languages/OharaYTEmbed.spanish_latin.php
@@ -3,22 +3,23 @@
 /*
  * @package Ohara Youtube Embed mod
  * @version 1.3
- * @author Jessica Gonz&aacute;lez <missallsunday@simplemachines.org>
- * @copyright Copyright (C) 2014 Jessica Gonz&aacute;lez
+ * @author Jessica Gonzalez <missallsunday@simplemachines.org>
+ * @copyright Copyright (C) 2014 Jessica Gonzalez
  * @license http://www.mozilla.org/MPL/MPL-1.1.html
  */
 
 global $txt;
 
-$txt['OharaYTEmbed_enable'] = 'Activar el mod Ohara Youtube Embed';
-$txt['OharaYTEmbed_enable_sub'] = 'Esta es la configuración principal, necasita estar activada para que el mod funcione correctamente.';
-$txt['OharaYTEmbed_autoEmbed'] = 'Activar la auto conversi&oacute;n';
-$txt['OharaYTEmbed_autoEmbed_sub'] = 'Si se activa esta opci&oacute;n todos los enlaces  de youtube y vimeo que sean v&aacute;lidos ser&aacute;n convertidos a videos. Si se desactiva deber&aacute;s de usar los tags correspondientes para convertir los enlaces en videos';
-$txt['OharaYTEmbed_width'] = 'Ancho del video:';
-$txt['OharaYTEmbed_width_sub'] = 'Si se deja vacio se usar&aacute; el valor por deault: 420.';
+$txt['OharaYTEmbed_enable'] = 'Activar el mod Ohara (auto)Embed';
+$txt['OharaYTEmbed_enable_sub'] = 'Esta es la opci&oacute;n principal, necesitas activar esta opci&oacute;n para que el mod funcione correctamente.';
+$txt['OharaYTEmbed_autoEmbed'] = 'Activar la opci&oacute;n autoembed';
+$txt['OharaYTEmbed_autoEmbed_sub'] = 'Si se activa, todos los enlaces que el mod considere v&aacute;lidos ser&aacute;n convertidos a videos sin necesidad de usar las etiquetas BBC.';
+$txt['OharaYTEmbed_width'] = 'Anchura del video:';
+$txt['OharaYTEmbed_width_sub'] = 'Si se deja vacio el mod usar&aacute; el valor por defecto: 420.';
 $txt['OharaYTEmbed_height'] = 'Altura del video:';
-$txt['OharaYTEmbed_height_sub'] = 'Si se deja vacio se usar&aacute; el valor por defecto: 315';
-$txt['OharaYTEmbed_desc'] = 'Inserta un video desde youtube';
-$txt['OharaYTEmbed_vimeo_desc'] = 'Inserta un video desde vimeo';
-$txt['OharaYTEmbed_unvalid_link'] = 'No es una dirección de %s v&aacute;lida';
-$txt['OharaYTEmbed_title'] = 'Ohara Youtube|Vimeo Embed mod';
+$txt['OharaYTEmbed_height_sub'] = 'i se deja vacio el mod usar&aacute; el valor por defecto: 315';
+$txt['OharaYTEmbed_invalid_link'] = 'No es una direcci&oacute;n de {site} v&aacute;lida';
+$txt['OharaYTEmbed_title'] = 'Ohara Embed mod';
+$txt['OharaYTEmbed_disabled_generic'] = 'El sitio {site} est&aacute; desactivado';
+$txt['OharaYTEmbed_enable_generic'] = 'Activar {site}';
+$txt['OharaYTEmbed_desc_generic'] = 'Colocar un video de {site}';
