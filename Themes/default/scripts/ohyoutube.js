a586997c99a6fe1730727b35d028ee1b80de714e
Suki <suki@missallsunday.com>
2015-04-04 13:19:19 -0600

    Improve video handling and image thumbnails
    Signed-off-by: Suki <suki@missallsunday.com>

--- Themes/default/scripts/ohyoutube.js
+++ Themes/default/scripts/ohyoutube.js
@@ -3,5 +3,85 @@
  @license http://www.mozilla.org/MPL/MPL-1.1.html
 */
 
-$(function(){function b(){$(".youtube").each(function(){$(this).css("background-image","url(http://i.ytimg.com/vi/"+this.id+"/sddefault.jpg)");$(this).append($("<div/>",{"class":"youtube_play"}));$(document).on("click","#"+this.id,function(a){a="https://www.youtube.com/embed/"+this.id+"?autoplay=1&autohide=1";$(this).data("params")&&(a+="&"+$(this).data("params"));a=$("<iframe/>",{frameborder:"0",src:a,width:$(this).width(),height:$(this).height()});$(this).replaceWith(a)})})}b();var c=function(a){for(var b=
-window.location.search.substring(1).split(";"),d=0;d<b.length;d++){var c=b[d].split("=");if(c[0]==a)return c[1]}}("action");if(c&&"post"==c)$("input[name=preview]").on("click",function(){setInterval(function(){b()},3E3)})});
+	function oh_main(){
+
+		$('.youtube').each(function() {
+
+			var videoID = this.id.replace('oh_',''),
+				imgsrc = oh_getImage(videoID),
+				imgHeight = $(this).height(),
+				imgWidth = $(this).width();
+
+			if (typeof imgsrc !== 'undefined'){
+				$(this).css({'background-image': 'url('+ imgsrc +')', 'background-size': 'cover'});
+			}
+
+			$(this).append($('<div/>', {'class': 'youtube_play'}));
+
+			$('#oh_'+videoID).on('click', function() {
+				var iframe_url = 'https://www.youtube.com/embed/' + videoID + '?autoplay=1&autohide=1';
+				if ($(this).data('params')) iframe_url+='&'+$(this).data('params');
+
+				// The height and width of the iFrame should be the same as parent
+				var iframe = $('<iframe/>', {'frameborder': '0', 'src': iframe_url, 'width': imgWidth, 'height': imgHeight })
+
+				// Replace the YouTube thumbnail with YouTube HTML5 Player
+				$(this).replaceWith(iframe);
+			});
+		});
+	};
+
+	function oh_getImage(youtubeID)
+	{
+		var imgsrc = '',
+			index, len,
+			imageTypes = ['hqdefault', 'mqdefault', 'sddefault', 'maxresdefault'];
+		for (index = 0, len = imageTypes.length; index < len; ++index) {
+			imgsrc = 'http://i.ytimg.com/vi/'+ youtubeID +'/'+ imageTypes[index] +'.jpg';
+
+			if (imgsrc.width !=0){
+				break;
+			}
+		}
+
+		// Still no image, show the default one
+		if (imgsrc.width ==0){
+			imgsrc = 'http://i.ytimg.com/vi/'+ youtubeID +'/default.jpg';
+		}
+
+		return imgsrc;
+	};
+
+	function oh_getUrl(sParam)
+	{
+		var sPageURL = window.location.search.substring(1);
+
+		// SimpleSEF or pretty urls?
+		if (sPageURL.indexOf(sParam) > -1) {
+			return true;
+		}
+
+		var sURLVariables = sPageURL.split(';');
+		for (var i = 0; i < sURLVariables.length; i++)
+		{
+			var sParameterName = sURLVariables[i].split('=');
+			if (sParameterName[0] == sParam)
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	function oh_refresh()
+	{
+		setTimeout(function(){oh_main()},3E3);
+	}
+
+	oh_main();
+
+	if (oh_getUrl('post'))
+		$('input[name=preview]').on('click',function(){
+			oh_refresh();
+		});
