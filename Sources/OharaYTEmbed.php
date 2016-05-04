dfcb6d0adfeaa0288ec5bed776bbc81afa59faae
Jessica González <suki@missallsunday.com>
2016-05-04 11:24:19 -0500

    Don't offer a minimized file, SMF will already do that for you
    Signed-off-by: Jessica González <suki@missallsunday.com>

--- Sources/OharaYTEmbed.php
+++ Sources/OharaYTEmbed.php
@@ -179,7 +179,7 @@ class OharaYTEmbed extends Suki\Ohara
 
 		// The much needed css file.
 		loadCSSFile('oharaEmbed.css', array('force_current' => false, 'validate' => true, 'minimize' => true));
-		loadJavascriptFile('ohvideos.min.js', array('local' => true, 'default_theme' => true, 'defer' => true, 'minimize' => true));
+		loadJavascriptFile('ohvideos.js', array('local' => true, 'default_theme' => true, 'defer' => true, 'minimize' => true));
 
 		// Set a max width var to let the JS code know how to act and react!
 		addInlineJavascript('
